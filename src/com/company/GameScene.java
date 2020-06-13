31fa772449c55a77929ffad2a9c73e4a496ce343
Hitesh <Hitech.tech101@gmail.com>
2020-06-13 14:13:12 -0400

    Added GameScene and brick Object
    still working

---
+++ src/com/company/GameScene.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+
+public class GameScene extends JFrame {
+
+
+    public GameScene(String gameName) {
+        super(gameName);
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,62 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.ColorModel;
+import java.math.BigInteger;
+
 public class Main {
 
+    private final String GAME_TITLE = "Curved Breakout";
+    private Rectangle gameBounds;
+    private GameScene gameScene;
+
     public static void main(String[] args) {
-	// write your code here
+        Main gameRunner = new Main();
+        gameRunner.startGame();
     }
+
+    public void startGame() {
+        buildScene();
+    }
+
+    public void buildScene(){
+        gameScene = new GameScene(GAME_TITLE);
+        gameScene.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameScene.setBounds(getGameBounds());
+        gameScene.setVisible(true);
+    }
+
+    public Rectangle getGameBounds() {
+        Rectangle virtualBounds = new Rectangle();
+        GraphicsEnvironment ge = GraphicsEnvironment.
+                getLocalGraphicsEnvironment();
+        GraphicsDevice[] gs =
+                ge.getScreenDevices();
+        for (int j = 0; j < gs.length; j++) {
+            GraphicsDevice gd = gs[j];
+            GraphicsConfiguration[] gc =
+                    gd.getConfigurations();
+            for (int i=0; i < gc.length; i++) {
+                virtualBounds =
+                        virtualBounds.union(gc[i].getBounds());
+            }
+        }
+        gameBounds = virtualBounds;
+        return virtualBounds;
+    }
+
+    public static class BrickGenerator {
+
+
+        public class Bricks extends Rectangle {
+            
+
+            public Bricks(int x, int y, int width, int height) {
+                super(x, y, width, height);
+            }
+        }
+    }
+
 }
